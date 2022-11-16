2ecadd816dd05d7c081b8c063c615869cc32be47
Bykov Denis Stanislavovich <stivenrain@gmail.com>
2022-11-16 22:13:31 +0300

    Some shabges config

---
+++ @dbykov/Label/dist/index.d.ts
@@ -0,0 +1,10 @@
+import React$1 from 'react';
+
+type TLabel = {
+	color?: string,
+	children?: React.ReactElement
+}
+
+declare const Label: React$1.FunctionComponent<TLabel>;
+
+export { Label as default };
